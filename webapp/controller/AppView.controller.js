0861775481a166038572ed3f6c37d6756a87e707
Manjunath V <blackhat6003@gmail.com>
2020-08-17 08:53:11 +0000

    Initial Finance View

---
+++ webapp/controller/AppView.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("financ.com.financial.controller.AppView", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"financ/com/financial/test/unit/controller/AppView.controller"
+], function () {
+	"use strict";
+});
